186325ab7d2a4b3c7c956c123a249eb20bddf38c
LekoArts <lekoarts@gmail.com>
2019-03-26 14:21:03 +0100

    add additional page in src/pages folder and connect both pages with gatsby's link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+export default () => <div>Hello world! <Link to="/page-2">Page 2</Link></div>
---
+++ src/pages/page-2.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => <div>This is the second page <Link to="/">Page 1</Link></div>
